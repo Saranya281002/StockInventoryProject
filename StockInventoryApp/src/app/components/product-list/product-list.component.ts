fd4f7a784a842e53bdc7d20cf1b4ad5044f925f1
Saranya Varma <saranya.varma@ufomoviez.com>
2026-04-21 15:46:09 +0530

    Pagination and search filter

--- StockInventoryApp/src/app/components/product-list/product-list.component.ts
+++ StockInventoryApp/src/app/components/product-list/product-list.component.ts
@@ -14,6 +14,10 @@ ngOnInit() {
   this.productService.getProducts().subscribe(data => {
     this.products = data;
     this.filteredProducts = data;
+
+    this.categories = [...new Set(data.map(p => p.category))];
+
+    this.updatePagination();
   });
 }
 
@@ -22,7 +26,10 @@ isEditMode = false;
 editProductId: number | null = null;
 searchText: string = '';
 selectedCategory: string = '';
-
+categories: string[] = [];
+currentPage: number = 1;
+itemsPerPage: number = 5;
+paginatedProducts: any[] = [];
 
 newProduct: any = {
   name: '',
@@ -82,11 +89,37 @@ deleteProduct(id: number) {
 }
 filterProducts() {
   this.filteredProducts = this.products.filter(p => {
-    const matchesSearch = p.name.toLowerCase().includes(this.searchText.toLowerCase());
-    const matchesCategory = this.selectedCategory 
-      ? p.category === this.selectedCategory 
-      : true;
+    const matchesSearch =
+      p.name.toLowerCase().includes(this.searchText.toLowerCase());
+
+    const matchesCategory =
+      this.selectedCategory === '' || p.category === this.selectedCategory;
+
     return matchesSearch && matchesCategory;
   });
+
+  this.currentPage = 1; // reset page
+  this.updatePagination();
+}
+clearFilters() {
+  this.searchText = '';
+  this.selectedCategory = '';
+  this.filteredProducts = this.products;
+  this.updatePagination();
+}
+updatePagination() {
+  const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+  const endIndex = startIndex + this.itemsPerPage;
+
+  this.paginatedProducts = this.filteredProducts.slice(startIndex, endIndex);
+}
+nextPage() {
+  this.currentPage++;
+  this.updatePagination();
+}
+
+prevPage() {
+  this.currentPage--;
+  this.updatePagination();
 }
 }
